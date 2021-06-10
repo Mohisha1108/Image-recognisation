3ade7c97fa8cc80b4680a9c3f5ccff1beeacf854
Mohisha1108 <73828119+Mohisha1108@users.noreply.github.com>
2021-06-10 15:53:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+//https://teachablemachine.withgoogle.com/models/aG8orcqAn/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+var camera = document.getElementById("camera");
+console.log(camera);
+Webcam.attach(camera);
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img src='"+data_uri+"' id='capture_image'>";
+    });
+}
+console.log('ml5 version: ',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aG8orcqAn/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("model is loaded")
+}
+function check()
+{
+    var img = document.getElementById("capture_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,result)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("resut_object_accuracy").innerHTML = result[0].confidence.toFixed(2);
+    }
+}
